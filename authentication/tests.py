65fd738808ca29aa87e2f9460877365fee16b77f
kramstyles <kramstyles@outlook.com>
2022-12-02 20:51:20 +0100

    created registration test

--- authentication/tests.py
+++ authentication/tests.py
@@ -11,13 +11,14 @@ class TestLogin(APITestCase):
         self.user = UserFactory()
         self.builder = FormBuilderFactory()
         self.admin = AdminFactory()
+        self.url = reverse('user-viewset-login')
         self.data = {
             'username': 'michael',
             'password': 'pass'
         }
 
     def test_to_login_accurately(self):
-        response = self.client.post(reverse('user-viewset-login'), data=self.data)
+        response = self.client.post(self.url, data=self.data)
         content = json.loads(response.content)
 
         self.assertTrue('michael', content.get('username'))
@@ -27,7 +28,7 @@ class TestLogin(APITestCase):
 
     def test_to_login_with_invalid_user(self):
         self.data['username'] = 'mark'
-        response = self.client.post(reverse('user-viewset-login'), data=self.data)
+        response = self.client.post(self.url, data=self.data)
         content = json.loads(response.content)
 
         self.assertTrue('User not found', content.get('username'))
@@ -36,10 +37,39 @@ class TestLogin(APITestCase):
 
     def test_to_login_with_wrong_credentials(self):
         self.data['password'] = 'mark'
-        response = self.client.post(reverse('user-viewset-login'), data=self.data)
+        response = self.client.post(self.url, data=self.data)
         content = json.loads(response.content)
 
         self.assertIn('Invalid', content.get('detail')[0])
         self.assertIn('detail', content)
         self.assertTrue(response.status_code, 400)
 
+
+class TestRegister(APITestCase):
+    def setUp(self) -> None:
+        self.user = UserFactory()
+        self.builder = FormBuilderFactory()
+        self.admin = AdminFactory()
+        self.url = reverse('user-viewset-register')
+        self.data = {
+            'username': 'timizuo',
+            'email': 'timizuo@formbuilder.com',
+            'password': 'pass'
+        }
+
+    def test_to_register_successfully(self):
+        response = self.client.post(self.url, data=self.data)
+        content = json.loads(response.content)
+
+        self.assertEqual(200, response.status_code)
+        self.assertIn('message', content)
+        self.assertEqual('User registered', content.get('message'))
+
+    def test_to_register_with_existing_contact(self):
+        self.data['username'] = 'admin'
+        response = self.client.post(self.url, data=self.data)
+        content = json.loads(response.content)
+
+        self.assertEqual(400, response.status_code)
+        self.assertIn('username', content)
+        self.assertIn('User exists', content.get('username')[0])
